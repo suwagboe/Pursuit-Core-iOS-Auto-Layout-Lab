53b3ef97a2b25e3e35870e4c5e0dbe834d649a7f
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-08 14:08:22 -0500

    made the first view control, trying to figure out how to pin them so when they are rotated to landscape they are pinned

---
+++ Auto Layout Lab/Auto Layout Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Lab
+//
+//  Created by World Domination a Brand on 11/8/19.
+//  Copyright © 2019 Pursuitful stuff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
